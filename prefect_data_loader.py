fff52c472681be79ed6f7c5fcbf9138b0efb98f8
qiongL910 <liuqiong891023@gmail.com>
2025-01-30 14:39:15 -0500

    fix a bug

--- prefect_data_loader.py
+++ prefect_data_loader.py
@@ -170,7 +170,7 @@ def ccdi_hub_data_loader(
     """
     print(sys.version)
     installed_packages = pkg_resources.working_set
-    installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages]):
+    installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
     print(installed_packages_list)
 
     print("Getting secrets from AWS Secrets Manager")
